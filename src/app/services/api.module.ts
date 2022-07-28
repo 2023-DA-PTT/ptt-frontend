521cfd4b760c1fe96aaad5aa0c9c35125418cc57
Niklas Aichinger <niklas.aichinger@dynatrace.com>
2022-07-28 15:07:50 +0200

    add auto generated service from openapi spec

---
+++ src/app/services/api.module.ts
@@ -0,0 +1,35 @@
+import { NgModule, ModuleWithProviders, SkipSelf, Optional } from '@angular/core';
+import { Configuration } from './configuration';
+import { HttpClient } from '@angular/common/http';
+
+import { InputArgumentResourceService } from './api/inputArgumentResource.service';
+import { OutputArgumentResourceService } from './api/outputArgumentResource.service';
+import { PlanResourceService } from './api/planResource.service';
+import { StepParameterRelationResourceService } from './api/stepParameterRelationResource.service';
+import { StepResourceService } from './api/stepResource.service';
+
+@NgModule({
+  imports:      [],
+  declarations: [],
+  exports:      [],
+  providers: []
+})
+export class ApiModule {
+    public static forRoot(configurationFactory: () => Configuration): ModuleWithProviders<ApiModule> {
+        return {
+            ngModule: ApiModule,
+            providers: [ { provide: Configuration, useFactory: configurationFactory } ]
+        };
+    }
+
+    constructor( @Optional() @SkipSelf() parentModule: ApiModule,
+                 @Optional() http: HttpClient) {
+        if (parentModule) {
+            throw new Error('ApiModule is already loaded. Import in your base AppModule only.');
+        }
+        if (!http) {
+            throw new Error('You need to import the HttpClientModule in your AppModule! \n' +
+            'See also https://github.com/angular/angular/issues/20575');
+        }
+    }
+}
