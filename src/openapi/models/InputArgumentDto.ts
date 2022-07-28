389e59cc598a82974a22f9b4e6617571b213e79a
Niklas Aichinger <niklas.aichinger@dynatrace.com>
2022-07-28 08:09:36 +0200

    create angular project

--- src/openapi/models/InputArgumentDto.ts
+++
@@ -1,9 +0,0 @@
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-export type InputArgumentDto = {
-    id?: number;
-    name?: string;
-    stepId?: number;
-};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */ 
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+    "./src/**/*.{html,ts}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
+  plugins: [],
 }
