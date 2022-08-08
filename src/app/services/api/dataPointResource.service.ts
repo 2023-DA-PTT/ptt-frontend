808f5cbe05d4d6fe884b7bbf35318da571f30fdb
Niklas Aichinger <niklas.aichinger@dynatrace.com>
2022-08-08 13:55:37 +0200

    update api services

--- src/app/services/api/dataPointResource.service.ts
+++ src/app/services/api/dataPointResource.service.ts
@@ -195,4 +195,64 @@ export class DataPointResourceService {
         );
     }
 
+    /**
+     * @param planRunId 
+     * @param stepId 
+     * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+     * @param reportProgress flag to report request and response progress.
+     */
+    public apiDatapointPlanrunPlanRunIdStepStepIdGet(planRunId: number, stepId: number, observe?: 'body', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<Array<DataPointDto>>;
+    public apiDatapointPlanrunPlanRunIdStepStepIdGet(planRunId: number, stepId: number, observe?: 'response', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<HttpResponse<Array<DataPointDto>>>;
+    public apiDatapointPlanrunPlanRunIdStepStepIdGet(planRunId: number, stepId: number, observe?: 'events', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<HttpEvent<Array<DataPointDto>>>;
+    public apiDatapointPlanrunPlanRunIdStepStepIdGet(planRunId: number, stepId: number, observe: any = 'body', reportProgress: boolean = false, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<any> {
+        if (planRunId === null || planRunId === undefined) {
+            throw new Error('Required parameter planRunId was null or undefined when calling apiDatapointPlanrunPlanRunIdStepStepIdGet.');
+        }
+        if (stepId === null || stepId === undefined) {
+            throw new Error('Required parameter stepId was null or undefined when calling apiDatapointPlanrunPlanRunIdStepStepIdGet.');
+        }
+
+        let localVarHeaders = this.defaultHeaders;
+
+        let localVarHttpHeaderAcceptSelected: string | undefined = options && options.httpHeaderAccept;
+        if (localVarHttpHeaderAcceptSelected === undefined) {
+            // to determine the Accept header
+            const httpHeaderAccepts: string[] = [
+                'application/json'
+            ];
+            localVarHttpHeaderAcceptSelected = this.configuration.selectHeaderAccept(httpHeaderAccepts);
+        }
+        if (localVarHttpHeaderAcceptSelected !== undefined) {
+            localVarHeaders = localVarHeaders.set('Accept', localVarHttpHeaderAcceptSelected);
+        }
+
+        let localVarHttpContext: HttpContext | undefined = options && options.context;
+        if (localVarHttpContext === undefined) {
+            localVarHttpContext = new HttpContext();
+        }
+
+
+        let responseType_: 'text' | 'json' | 'blob' = 'json';
+        if (localVarHttpHeaderAcceptSelected) {
+            if (localVarHttpHeaderAcceptSelected.startsWith('text')) {
+                responseType_ = 'text';
+            } else if (this.configuration.isJsonMime(localVarHttpHeaderAcceptSelected)) {
+                responseType_ = 'json';
+            } else {
+                responseType_ = 'blob';
+            }
+        }
+
+        return this.httpClient.get<Array<DataPointDto>>(`${this.configuration.basePath}/api/datapoint/planrun/${encodeURIComponent(String(planRunId))}/step/${encodeURIComponent(String(stepId))}`,
+            {
+                context: localVarHttpContext,
+                responseType: <any>responseType_,
+                withCredentials: this.configuration.withCredentials,
+                headers: localVarHeaders,
+                observe: observe,
+                reportProgress: reportProgress
+            }
+        );
+    }
+
 }
