9cc7e818369712ae79dd34fe5b6ee83d32201375
Niklas Aichinger <niklas.aichinger@dynatrace.com>
2022-08-08 12:24:08 +0200

    create dashboard components

---
+++ src/app/components/test-result/test-result-detail/test-result-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-result-detail',
+  templateUrl: './test-result-detail.component.html',
+  styleUrls: ['./test-result-detail.component.scss']
+})
+export class TestResultDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
