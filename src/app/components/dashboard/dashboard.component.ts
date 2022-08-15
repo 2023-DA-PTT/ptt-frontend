493777ab72b15b08ebdbb84eae6c5e5b5880db0e
S1mple133 <pavelescu_darius@yahoo.com>
2022-08-15 11:47:30 +0200

    added location to dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit(): void {
   }
 
+  formatUrlPath(urlPath: string) {
+    switch (urlPath) {
+      case "dashboard":
+        return "Dashboard";
+      case "test-plan-overview":
+        return "Test Plan Overview";
+    }
+
+    return urlPath;
+  }
 }
