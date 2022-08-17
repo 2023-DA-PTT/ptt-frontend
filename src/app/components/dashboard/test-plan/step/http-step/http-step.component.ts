4c7a66debca9549856333303406ac40ca73caae6
Niklas Aichinger <niklas.aichinger@dynatrace.com>
2022-08-17 12:45:06 +0200

    fix create http step

--- src/app/components/dashboard/test-plan/step/http-step/http-step.component.ts
+++ src/app/components/dashboard/test-plan/step/http-step/http-step.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { HttpStepDto, HttpStepResourceService, RequestContentType } from 'src/app/services';
 
@@ -10,6 +11,7 @@ import { HttpStepDto, HttpStepResourceService, RequestContentType } from 'src/ap
 })
 export class HttpStepComponent implements OnInit {
   
+  @Output() stepChange = new EventEmitter<HttpStepDto>();
   @Input()
   step: HttpStepDto = {};
   @Input()
@@ -18,7 +20,8 @@ export class HttpStepComponent implements OnInit {
   contentTypes: RequestContentType[] = [];
 
   constructor(private httpStepService: HttpStepResourceService,
-              private toastr: ToastrService) { }
+              private toastr: ToastrService,
+              private router: Router) { }
 
   ngOnInit(): void {
     this.contentTypes = Object.values(RequestContentType);
@@ -30,13 +33,29 @@ export class HttpStepComponent implements OnInit {
       return;
     }
 
-    this.httpStepService.apiPlanPlanIdStepStepIdHttpPut(this.planId, this.step.id!, this.step).subscribe({
-      next: newStep => {
-        this.toastr.success("Saved")
-      },
-      error: err => {
-        this.toastr.error("Error! ")
-      }
-    });
+    if (this.step.id == -1) {
+      this.httpStepService.apiPlanPlanIdStepHttpPost(this.planId, this.step).subscribe({
+        next: step => {
+          this.toastr.success("Success!")
+          this.step = step;
+          this.stepChange.emit(step);
+          this.router.navigate(["/dashboard/test-plan/"+this.planId+"/step/"+step.id]).then()
+        },
+        error: () => {
+          this.toastr.error("Error!")
+        }
+      });
+    } else {
+      this.httpStepService.apiPlanPlanIdStepStepIdHttpPut(this.planId, this.step.id!, this.step).subscribe({
+        next: step => {
+          this.step = step;
+          this.stepChange.emit(step);
+          this.toastr.success("Saved")
+        },
+        error: () => {
+          this.toastr.error()
+        }
+      });
+    }
   }
 }
