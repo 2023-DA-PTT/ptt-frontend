dca4c4976f89e74b2713234f05a1d0f6eab5914e
Niklas Aichinger <niklas.aichinger@dynatrace.com>
2022-08-17 08:55:34 +0200

    fill output parameter form and add update

--- src/app/components/dashboard/test-plan/step/output-parameters/output-parameters.component.ts
+++ src/app/components/dashboard/test-plan/step/output-parameters/output-parameters.component.ts
@@ -1,4 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import {
   InputArgumentDto,
   InputArgumentResourceService,
@@ -20,13 +22,37 @@ export class OutputParametersComponent implements OnInit {
   outputParameters: OutputArgumentDto[] = [];
   outputTypes: string[] = []
 
-  constructor(private outputParamService: OutputArgumentResourceService) { }
+  constructor(private outputParamService: OutputArgumentResourceService,
+              private toastr: ToastrService) { }
 
   ngOnInit(): void {
     this.outputTypes = Object.values(OutputType);
+
+    this.outputParamService.apiPlanPlanIdStepStepIdOutputArgumentGet(this.planId, this.stepId).subscribe(
+      (data) => this.outputParameters = data );
   }
 
   addNewOutputParam() {
+    console.log(this.outputParameters);
     this.outputParameters.push({id:-1, stepId: this.stepId})
+  }
+
+  onSubmit(outputParamForm: NgForm) {
+    if (!outputParamForm.valid) {
+      console.log("UNVALID")
+      return;
+    }
+
+    this.outputParameters.filter(p => p.name).filter(p => !p.id).forEach(p => {
+      this.outputParamService.apiPlanPlanIdStepStepIdOutputArgumentPost(this.planId, this.stepId, p).subscribe(p => {
+        this.toastr.success("Created Output Parameter " + p.name);
+      })
+    });
+
+    this.outputParameters.filter(p => p.name).filter(p => p.id).forEach(p => {
+      this.outputParamService.apiPlanPlanIdStepStepIdOutputArgumentPut(this.planId, this.stepId, p).subscribe(p => {
+        this.toastr.success("Updated Output Parameter " + p.name);
+      })
+    });
   }
 }
